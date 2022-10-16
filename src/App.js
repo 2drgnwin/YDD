aad18dda03b7cc77587d5275347161938b8f88e2
YSL <2drgnwin@gmail.com>
2022-10-16 14:30:39 +0900

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+const costObj = {
+  Brian: 5000,
+  Tom: 103000,
+  James: 24000,
+};
+const names = Object.keys(costObj);
+const name = 'Tom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <head></head>
+
+      <body>
+        hello html
+        <table class="table table-bordered">
+          <thead>
+            <tr class="table-primary">
+              <th>Name</th>
+              <th>Cost</th>
+            </tr>
+          </thead>
+          <tbody>
+            {names.map((name) => {
+              return (
+                <tr>
+                  <td>{name}</td>
+                  <td>{costObj[name]}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <button class="btn btn-primary">push</button>
+      </body>
     </div>
   );
 }
